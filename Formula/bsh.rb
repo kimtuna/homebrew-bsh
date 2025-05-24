d4b7a255e089f4dc3b3729d8c9df98348716c7f4
kdw240 <kdw240@gmail.com>
2025-05-24 16:54:35 +0900

    Remove amd64 version

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -8,9 +8,6 @@ class Bsh < Formula
     if Hardware::CPU.arm?
       url "https://github.com/kimtuna/bsh/releases/download/v0.1.0/bsh-darwin-arm64.tar.gz"
       sha256 "f8771fbe5d8058641f898d4b98dc91b707dfafe03ab9136dd40dfe6a7c2bacbb"
-    else
-      url "https://github.com/kimtuna/bsh/releases/download/v0.1.0/bsh-darwin-amd64.tar.gz"
-      sha256 "..." # 실제 sha256 값으로 교체 필요
     end
   end
 
