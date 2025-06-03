0616fab5dcc5e00ed0cc2743b31178c5a81bd018
kdw240 <kdw240@gmail.com>
2025-06-03 17:35:28 +0900

    fix: SHA256 해시 업데이트

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -3,7 +3,7 @@ class Bsh < Formula
   homepage "https://github.com/kimtuna/bsh"
   version "0.1.3"
   url "https://github.com/kimtuna/bsh/releases/download/v0.1.3/bsh-darwin-arm64.tar.gz"
-  sha256 "84299584d26e2357b4e3c35c695c44cd182fdcab7a48541a63bd5a488413d167"
+  sha256 "5acbd55a4bbef76d7fea3792d3a3b397f24c2efc3cbb8b95f0bcf7916cbbf64f"
   license "MIT"
 
   def install
