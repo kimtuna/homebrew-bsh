e89b26a9862600f8aedb49a7cd7aeb54f798fb23
kdw240 <kdw240@gmail.com>
2025-06-03 17:36:23 +0900

    fix: GitHub 파일의 실제 SHA256 해시로 업데이트

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -3,7 +3,7 @@ class Bsh < Formula
   homepage "https://github.com/kimtuna/bsh"
   version "0.1.3"
   url "https://github.com/kimtuna/bsh/releases/download/v0.1.3/bsh-darwin-arm64.tar.gz"
-  sha256 "5acbd55a4bbef76d7fea3792d3a3b397f24c2efc3cbb8b95f0bcf7916cbbf64f"
+  sha256 "6f2e3301d4e5e066f39c7a311c590595986ef80cbd9e1cc30aca86a3deb0d465"
   license "MIT"
 
   def install
