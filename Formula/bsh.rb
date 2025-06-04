2e41a89bc88dcc5882bcc938df4e3b819d9cb73b
kdw240 <kdw240@gmail.com>
2025-06-04 20:34:48 +0900

    feat: bsh formula 업데이트 (v0.1.4)

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -1,10 +1,12 @@
 class Bsh < Formula
-  desc "Blockchain Server Hosting CLI"
+  desc "Blockchain Server Hosting CLI tool"
   homepage "https://github.com/kimtuna/bsh"
-  version "0.1.3"
-  url "https://github.com/kimtuna/bsh/releases/download/v0.1.3/bsh-darwin-arm64.tar.gz"
-  sha256 "6f2e3301d4e5e066f39c7a311c590595986ef80cbd9e1cc30aca86a3deb0d465"
-  license "MIT"
+  version "0.1.4"
+  
+  on_macos do
+    url "https://github.com/kimtuna/bsh/releases/download/v0.1.4/bsh-darwin-arm64.tar.gz"
+    sha256 "566462f87613c01d71018306902e5d063e7dc1f2fb623b78d0d16a0981f4a1f5"
+  end
 
   def install
     bin.install "bsh"
