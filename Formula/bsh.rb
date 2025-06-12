d9bb95389ef49f57bc3ecff33187465738deeb0b
kdw240 <kdw240@gmail.com>
2025-06-12 18:22:30 +0900

    Update bsh to v0.1.5

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -1,13 +1,13 @@
 class Bsh < Formula
   desc "Blockchain Server Hosting CLI"
   homepage "https://github.com/kimtuna/bsh"
-  version "0.1.3"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kimtuna/bsh/releases/download/v0.1.0/bsh-darwin-arm64.tar.gz"
-      sha256 "f8771fbe5d8058641f898d4b98dc91b707dfafe03ab9136dd40dfe6a7c2bacbb"
+      url "https://github.com/kimtuna/bsh/releases/download/v0.1.5/bsh-darwin-arm64.tar.gz"
+      sha256 "b44753217e1598c021649a403bf79cc479c8dbeec24aa9338ca615647a9a192c"
     end
   end
 
